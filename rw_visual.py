8d6caffeb10ed461d65a5080ea3c625e6ce7c8de
CHENGZ5114 <42401193+CHENGZ5114@users.noreply.github.com>
2018-08-30 15:13:28 +0800

    Add files via upload

---
+++ rw_visual.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+from random_walk import RandomWalk
+while True:
+	rw=RandomWalk()
+	rw.fill_walk()
+	plt.scatter(rw.x_values,rw.y_values,s=15)
+	plt.show()
+
+	keep_running=input('make snother walk?(y/n):')
+	if keep_running=='n':
+		break
