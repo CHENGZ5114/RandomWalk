5522c5ab1faa41573a8c48f07fc02a99344a44b4
CHENGZ5114 <42401193+CHENGZ5114@users.noreply.github.com>
2018-08-30 15:47:36 +0800

    Add files via upload

--- rw_visual.py
+++ rw_visual.py
@@ -3,8 +3,12 @@ from random_walk import RandomWalk
 while True:
 	rw=RandomWalk()
 	rw.fill_walk()
+	
 	point_numbers=list(range(rw.num_points))
 	plt.scatter(rw.x_values,rw.y_values,c=point_numbers,cmap=plt.cm.Blues,edgecolor='none',s=15)
+	plt.axes().get_xaxis().set_visible(False)
+	plt.axes().get_yaxis().set_visible(False)
+	
 	plt.scatter(rw.x_values,rw.y_values,s=15)
 	plt.show()
 
